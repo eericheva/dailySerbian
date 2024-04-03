c73f15d9ed87344184ea759d8f6a276f434afa36
eericheva <epifana89@gmail.com>
2024-04-03 21:02:59 +0200

    add heroku

--- utils/setup.py
+++ utils/setup.py
@@ -2,23 +2,34 @@ import glob
 import json
 import os
 
+# import fastapi
 import telebot
 from telegram.ext import Updater
 from telegram_bot_token import TOKEN
 
 dailySerbian_bot = telebot.TeleBot(TOKEN)
-dailySerbian_updater = Updater(TOKEN, use_context=True)
-dailySerbian_dispatcher = dailySerbian_updater.dispatcher
+dailySerbian_bot.remove_webhook()
+# dailySerbian_app = fastapi.FastAPI(docs=None, redoc_url=None)
+
+# Set webhook
+# WEBHOOK_HOST = "national-beetle-super.ngrok-free.app"
+# WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+# WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+# WEBHOOK_URL_PATH = "/{}/".format(TOKEN)
+
+# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
 this_project_path = os.getcwd()  # os.path.dirname(os.path.abspath(__file__))
 JSON_SCHEMA_PATH = os.path.join(this_project_path, "utils/basemodel_dailySerbian.json")
 json_datamodel_path = os.path.join(this_project_path, "utils/basemodel_dailySerbian.py")
-os.system(
-    f"datamodel-codegen  --input {JSON_SCHEMA_PATH} "
-    f"--input-file-type jsonschema "
-    f"--output {json_datamodel_path}  "
-    f"--target-python-version 3.8 --use-default"
-)
+if not os.path.isfile(json_datamodel_path):
+    os.system(
+        f"datamodel-codegen  --input {JSON_SCHEMA_PATH} "
+        f"--input-file-type jsonschema "
+        f"--output {json_datamodel_path}  "
+        f"--target-python-version 3.8 --use-default"
+    )
 
 USER_DICT_PATH = os.path.join(this_project_path, "data")
 my_user_ends = ".usr.json"
@@ -47,3 +58,6 @@ CURRENT_USER_DICT = lambda message: json.load(
 )
 
 MAX_DICT_LEN = 30
+
+dailySerbian_updater = Updater(TOKEN, use_context=True)
+dailySerbian_dispatcher = dailySerbian_updater.dispatcher
