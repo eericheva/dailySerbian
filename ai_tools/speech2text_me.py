bdf9cffaa5b10d992dcab1c88c9bbbeff77ac775
eericheva <epifana89@gmail.com>
2024-04-04 18:05:51 +0200

    ffbrobe add heroku

--- ai_tools/speech2text_me.py
+++ ai_tools/speech2text_me.py
@@ -4,10 +4,10 @@ import speech_recognition
 
 
 def speech2text_me(file_path):
-    f = io.BytesIO(file_path)
-    f = pydub.AudioSegment.from_file(f)
-    f = f.export(format="wav")
-    # f = pydub.AudioSegment.from_file(io.BytesIO(file_path)).export(format="wav")
+    # f = io.BytesIO(file_path)
+    # f = pydub.AudioSegment.from_file(f)
+    # f = f.export(format="wav")
+    f = pydub.AudioSegment.from_file(io.BytesIO(file_path)).export(format="wav")
     recognator = speech_recognition.Recognizer()
     with speech_recognition.AudioFile(f) as source:
         audio_data = recognator.record(source)
